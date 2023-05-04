47c0c7659b883e08f99793a7ce109a873feee3d3
ivan <sakotaivan81@gmail.com>
2023-05-04 16:38:55 +0200

    moj prvi commit

---
+++ js.js
@@ -0,0 +1,18 @@
+/*var niz = [2, 4, 5, 6, 7, 9];
+function ante(niz) {
+  for (var i = 0; i < niz.length; i++) {
+    if (paran(niz[i])) {
+      console.log(i);
+    }
+  }
+}
+function paran(x) {
+  if (x % 2 == 0) {
+    return x;
+  }
+}
+
+ante(niz);
+*/
+
+console.log("oooo");
